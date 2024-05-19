a229a451b508c5223d233c27d1235edadb803b34
Jose luis <josecahuanag@gmail.com>
2024-05-19 00:40:31 -0500

    New Change

--- src/assets/components/SneakerPage.jsx
+++ src/assets/components/SneakerPage.jsx
@@ -1,20 +1,25 @@
+import { Link } from 'react-router-dom';
+import './SneakerPage.css'
+
 export const SneakerPage = ({ data }) => {
     return (
         <>
-            <section>
-                <img src="" alt="" />
-                <div>
-                    
-                </div>
-            </section>
-            <section>
-                <div>
-                <p>{marca}</p>
-                <p>Codigo:{Math.radom}</p>
-                </div>
-                <h3>{nombre}</h3>
+            <Link>
+                <section className="snaker__img">
+                    <img src="" alt="" />
+                    <div>
 
-            </section>
+                    </div>
+                </section>
+                <section>
+                    <div>
+                        <p>{marca}</p>
+                        <p>Codigo:{Math.radom}</p>
+                    </div>
+                    <h3>{nombre}</h3>
+
+                </section>
+            </Link>
         </>
     )
 }
