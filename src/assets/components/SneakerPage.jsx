0e4f928188c81cad1124631af8b832e6ce6e9218
Jose luis <josecahuanag@gmail.com>
2024-05-18 20:30:39 -0500

    modifed

---
+++ src/assets/components/SneakerPage.jsx
@@ -0,0 +1,20 @@
+export const SneakerPage = ({ data }) => {
+    return (
+        <>
+            <section>
+                <img src="" alt="" />
+                <div>
+                    
+                </div>
+            </section>
+            <section>
+                <div>
+                <p>{marca}</p>
+                <p>Codigo:{Math.radom}</p>
+                </div>
+                <h3>{nombre}</h3>
+
+            </section>
+        </>
+    )
+}
