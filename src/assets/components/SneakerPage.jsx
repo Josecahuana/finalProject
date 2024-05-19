9c59951bce4b0dc7f9536591dee4d9d8def38b18
Jose luis <josecahuanag@gmail.com>
2024-05-19 02:19:44 -0500

    Modified SneakerPage in list with useParams

--- src/assets/components/SneakerPage.jsx
+++ src/assets/components/SneakerPage.jsx
@@ -1,26 +1,31 @@
 import { useParams } from 'react-router-dom';
+import sneakers from './data.js'
 import './SneakerPage.css'
+import { useEffect, useState } from 'react';
 
-export const SneakerPage = ({ data }) => {
+export const SneakerPage = () => {
+   const {id} = useParams();
 
-    let params = useParams();
+   const newData = sneakers.find( sneaker => sneaker.id === +id)
+
     return (
         <>
-
+            <div className='sneaker__div'>
                 <section className="snaker__img">
                     <img src="" alt="" />
                     <div>
-                        {params.id}
+                        {newData.marca}
                     </div>
                 </section>
                 <section>
                     <div>
-                        <p></p>
+                        <p>{newData.nombre} </p>
                         <p>Codigo</p>
                     </div>
                     <h3></h3>
 
                 </section>
+            </div>
         </>
     )
 }
