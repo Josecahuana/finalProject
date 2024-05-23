1a78d7bd69215bb3928aaf5a50a487e02876732a
Jose luis <josecahuanag@gmail.com>
2024-05-22 19:23:19 -0500

    Modify change

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,7 +1,11 @@
+import { useEffect, useState } from "react"
+
 // import HeaderStyle from './HeaderStyle.css'
 export const Header = ({ cart }) => {
 
-    console.log(cart);
+    const [change, setChange] = useState(cart);
+
+
 
     return (
         <>
@@ -28,13 +32,14 @@ export const Header = ({ cart }) => {
                     <span className="sesion__cart">
                         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" /></svg>
                         <span>{cart.length}</span>
-                        <div>
+                        <div className="shops">
                             {
-                                cart.map((car, index) => {
+                                cart.map((car) => {
                                     return (
-                                        <div key={index}>
+                                        <div key={car.id} className="shops__item">
                                             <p>{car.nombre}</p>
-                                            <p>{car.precio}</p>
+                                            <span>{car.precio}</span>
+                                            <button type="button">Delete</button>
                                         </div>
                                     )
                                 })
